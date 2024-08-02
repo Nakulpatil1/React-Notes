db5d40f044f25050e47f9d87197eadd9994da6ee
Nakulpatil1 <r3verbit@gmail.com>
2024-08-02 21:20:57 +0530

    Update

--- 01.ReactHooks/src/App.jsx
+++ 01.ReactHooks/src/App.jsx
@@ -3,9 +3,10 @@ import "./App.css"
 
 function App() {
   
-  const [counter, setcounter] = useState(0)     //useState Is Used to Store and Update in a component on UI as well.
+ const [counter, setcounter] = useState(0)     //useState Is Used to Store and Update in a component on UI as well.
+
   //  let counter = 10;
- const addValue =  () => {
+ const addValue =  function() {
   // counter = counter + 1
   if (counter < 20) {
     setcounter(counter + 1)
@@ -19,12 +20,12 @@ const removeValue = function () {
 };
 
    return (
-    <><h1>Hello Nakul</h1>
+    <>
+    <h1>Hello Nakul</h1>
     <h2>Counter:{counter}</h2>
     <button onClick={addValue} >Add Value</button><br /><br />
     <button onClick={removeValue}> Remove Value</button>
     </>
-    
   )
 }
 export default App
